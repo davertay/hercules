07593723b029b0b10efa1dffc0510d6f2240160f
davertay <davertay@gmail.com>
2026-05-24 12:33:55 -0700

    Initial project

---
+++ Hercules/HerculesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerculesApp.swift
+//  Hercules
+//
+//  Created by David Taylor on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct HerculesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HerculesTests/HerculesTests.swift
@@ -0,0 +1,17 @@
+//
+//  HerculesTests.swift
+//  HerculesTests
+//
+//  Created by David Taylor on 5/24/26.
+//
+
+import Testing
+@testable import Hercules
+
+struct HerculesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
